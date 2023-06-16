e5cce57e85c5593e27243d377cd762f9afbf943c
mmc-servel <103067540+mmc-servel@users.noreply.github.com>
2023-06-17 00:50:51 +0300

    Update buildReact.sh

--- backend/buildReact.sh
+++ backend/buildReact.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+export M2_HOME=/opt/apache-maven-3.9.2
+export M2=/opt/apache-maven-3.9.2/bin
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.19.0.7-4.0.1.el8.x86_64
+
+export PATH=$PATH:$HOME/bin:$M2:$JAVA_HOME/bin
+
+export PATH
+
 /usr/bin/pkill -f with-dependencies
 pwd
 echo $1
