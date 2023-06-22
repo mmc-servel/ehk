271b19173fd0e05b495b79377deab4f4cf8523f1
Sergiu Velescu <Sergiu.Velescu@endava.com>
2023-06-22 19:33:36 +0300

    Fix jar/flyway migration

--- backend/buildReact.cmd
+++ backend/buildReact.cmd
@@ -3,6 +3,6 @@ set NODE_OPTIONS=--openssl-legacy-provider
 
 rem TO DO
 rem The script doesn't run both npm lines - I don't know why - it runs only one yhat is why I commented the 'npm install'
-npm install
+rem npm install
 npm run build
 
